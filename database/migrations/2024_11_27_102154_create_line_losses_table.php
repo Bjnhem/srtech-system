34094c4200223b9d544da30e1e67b2245e6e318a
Binh em <bjnhzuu2@gmail.com>
2024-12-02 08:10:48 +0700

    2.12 update sql

--- database/migrations/2024_11_27_102154_create_line_losses_table.php
+++ database/migrations/2024_11_27_102154_create_line_losses_table.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,9 +12,10 @@ class CreateLineLossesTable extends Migration
             $table->id(); // ID cho mỗi dòng loss
             $table->foreignId('plan_id')->constrained('plans')->onDelete('cascade'); // Kết nối với bảng plans
             $table->foreignId('error_list_id')->constrained('errors_list')->onDelete('cascade'); // Kết nối với bảng errors_list
-            $table->string('loss_type'); // Loại lỗi (ví dụ: thiếu nguyên liệu, lỗi máy móc)
+            $table->string('Code_ID'); // Loại lỗi (ví dụ: thiếu nguyên liệu, lỗi máy móc)
+            $table->integer('prod_qty'); // Loại lỗi (ví dụ: thiếu nguyên liệu, lỗi máy móc)
+            $table->integer('NG_qty'); // Loại lỗi (ví dụ: thiếu nguyên liệu, lỗi máy móc)
             $table->text('remark'); // Ghi chú thêm về lỗi
-            $table->integer('loss_amount'); // Số lượng mất mát
             $table->string('time_slot'); // Khung giờ
             $table->timestamps(); // Thời gian tạo và cập nhật
         });
@@ -24,4 +26,3 @@ class CreateLineLossesTable extends Migration
         Schema::dropIfExists('line_losses');
     }
 }
-
