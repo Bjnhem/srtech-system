4fe9c01f36e628fd95eaff0e6c6cee9011ba6a97
Binh em <bjnhzuu2@gmail.com>
2024-12-26 14:18:49 +0700

    1

--- resources/views/srtech/WareHouse/layouts/WareHouse_layout.blade.php
+++ resources/views/srtech/WareHouse/layouts/WareHouse_layout.blade.php
@@ -138,13 +138,7 @@
         </footer>
     </main>
 
-
-    @include('srtech.layouts._scripts')
-
-    <script src="{{ asset('checklist-ilsung/js/select-boxes.js') }}"></script>
-    <script src="{{ asset('laravel-filemanager/js/stand-alone-button.js') }}"></script>
-    <script src="{{ asset('jquery-ui/auto.js') }}"></script>
-    {{-- <script>
+    <script>
         document.addEventListener("DOMContentLoaded", function() {
             const notificationList = document.getElementById("notification-list");
             const notificationBadge = document.querySelector("#notification-drop .dots");
@@ -193,7 +187,29 @@
                 })
                 .catch(error => console.error('Error fetching notifications:', error));
         });
-    </script> --}}
+    </script>
+
+    {{-- @include('srtech.layouts._scripts') --}}
+
+    <!-- Backend Bundle JavaScript -->
+    <script src="{{ asset('js/libs.min.js') }}"></script>
+    <script src="{{ asset('js/hope-ui.js') }}"></script>
+
+
+
+    {{-- <script src="{{ asset('js/plugins/circle-progress.js') }}"></script> --}}
+    <script src="{{ asset('checklist-ilsung/js/toastr.min.js') }}"></script>
+
+    <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
+    <script src="{{ asset('smart-ver2/js/components/datepicker.js') }}"></script>
+    <script src="{{ asset('laravel-filemanager/js/stand-alone-button.js') }}"></script>
+    <script src="{{ asset('jquery-ui/auto.js') }}"></script>
+    <script src="{{ asset('smart-ver2/DataTables/jQuery-3.7.0/jquery-3.7.0.min.js') }}"></script>
+    <script src="{{ asset('smart-ver2/DataTables/datatables.min.js') }}"></script>
+    <script src="{{ asset('checklist-ilsung/js/select-boxes.js') }}"></script>
+
+
+
 
     @yield('admin-js')
 </body>
